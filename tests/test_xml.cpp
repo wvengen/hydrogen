4ef7c4c910379e4d5858e281b8ee6cee62d93af2
Jeremy Zurcher <jeremy@asynk.ch>
2010-12-08 10:06:12 +0000

    add Drumkit load/save test
    git-svn-id: http://svn.assembla.com/svn/hydrogen/branches/jeremyz@1972 3b3fb362-3133-0410-aa15-cf69e0a59cb7

--- tests/test_xml.cpp
+++ tests/test_xml.cpp
@@ -1,4 +1,19 @@
 
-int main(int argc, char *argv[]) {
-    return 0;
+#include <hydrogen/basics/drumkit.h>
+
+int xml_drumkit( int log_level ) {
+
+    ___INFOLOG( "test xml drumkit validation, read and write" );
+
+    H2Core::Drumkit* dk = 0;
+    dk = H2Core::Drumkit::load_file( "./tests/data/drumkit.xml" );
+    if( !dk ) { return EXIT_FAILURE; }
+    dk->dump();
+    dk->save( "./tests/data/drumkit2.xml", true );
+    delete dk;
+    dk = 0;
+    dk = H2Core::Drumkit::load_file( "./tests/data/drumkit2.xml" );
+    if( !dk ) { return EXIT_FAILURE; }
+
+    return EXIT_SUCCESS;
 }
---
+++ tests/tests.cpp
@@ -0,0 +1,24 @@
+
+#include "hydrogen/logger.h"
+#include "hydrogen/Object.h"
+#include "hydrogen/helpers/filesystem.h"
+
+int xml_drumkit( int log_level );
+
+int main(int argc, char *argv[]) {
+
+    int log_level = Logger::Info | Logger::Warning | Logger::Error;
+    /* Logger */
+    Logger* logger = Logger::bootstrap( log_level );
+    /* Object */
+    Object::bootstrap( logger, logger->should_log(Logger::Debug) );
+    /* Filesystem */
+    H2Core::Filesystem::bootstrap( logger );
+    H2Core::Filesystem::info();
+
+    xml_drumkit( log_level );
+
+    sleep(2);
+
+    return EXIT_SUCCESS;
+}
