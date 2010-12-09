bd5cbfdb5771d8bbd33c842d91b6699566d7416f
Jeremy Zurcher <jeremy@asynk.ch>
2010-12-09 20:50:16 +0000

    update drumkit tests
    git-svn-id: http://svn.assembla.com/svn/hydrogen/branches/jeremyz@1986 3b3fb362-3133-0410-aa15-cf69e0a59cb7

--- tests/test_xml.cpp
+++ tests/test_xml.cpp
@@ -6,15 +6,32 @@ int xml_drumkit( int log_level ) {
 
     ___INFOLOG( "test xml drumkit validation, read and write" );
 
-    H2Core::Drumkit* dk = 0;
-    dk = H2Core::Drumkit::load_file( "./tests/data/drumkit.xml" );
-    if( !dk ) { return EXIT_FAILURE; }
-    dk->dump();
-    dk->save( "./tests/data/drumkit2.xml", true );
-    delete dk;
-    dk = 0;
-    dk = H2Core::Drumkit::load_file( "./tests/data/drumkit2.xml" );
-    if( !dk ) { return EXIT_FAILURE; }
+    H2Core::Drumkit* dk0 = 0;
+    H2Core::Drumkit* dk1 = 0;
+    H2Core::Drumkit* dk2 = 0;
+
+    dk0 = H2Core::Drumkit::load( "./tests/data/drumkit" );
+    if( !dk0 ) { return EXIT_FAILURE; }
+    dk0->dump();
+    if( !dk0->load_samples() ) { return EXIT_FAILURE; }
+    dk0->save( "./tests/data/drumkit1.xml", true );
+
+    dk1 = H2Core::Drumkit::load_file( "./tests/data/drumkit1.xml" );
+    dk1->dump();
+    if( !dk1 ) { return EXIT_FAILURE; }
+
+    dk2 = new H2Core::Drumkit( dk1 );
+    dk2->dump();
+    dk2->set_name("COPY");
+    if( !dk2 ) { return EXIT_FAILURE; }
+    dk2->save( "./tests/data/drumkit2.xml", true );
+
+    delete dk0;
+    delete dk1;
+    dk2->dump();
+    delete dk2;
+
+    H2Core::Filesystem::rm( "./tests/data/drumkit1.xml" );
     H2Core::Filesystem::rm( "./tests/data/drumkit2.xml" );
 
     return EXIT_SUCCESS;
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -7,7 +7,7 @@ int xml_drumkit( int log_level );
 
 int main(int argc, char *argv[]) {
 
-    int log_level = Logger::Info | Logger::Warning | Logger::Error;
+    int log_level = Logger::Debug | Logger::Info | Logger::Warning | Logger::Error;
     /* Logger */
     Logger* logger = Logger::bootstrap( log_level );
     /* Object */
@@ -19,6 +19,8 @@ int main(int argc, char *argv[]) {
     xml_drumkit( log_level );
 
     sleep(2);
+
+    delete logger;
 
     return EXIT_SUCCESS;
 }
